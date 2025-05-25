b76a76df4788279a09a99572d828b51cf72b21b6
Alex F <aleksey.fedotov@gmail.com>
2025-05-25 12:28:30 +0500

    Remove color material enum

--- src/scene/materials/material.rs
+++ src/scene/materials/material.rs
@@ -1,16 +1,16 @@
 use super::super::components::{Camera, Transform};
 use super::super::Assets;
-use super::color::ColorMaterial;
 use super::post_process::PostProcessMaterial;
 use super::skybox::SkyboxMaterial;
 use super::textured::TexturedMaterial;
+use super::uniforms::{Vec3Uniform, WorldViewProjUniform};
 use crate::math::Vec3;
 use crate::render;
-use crate::render::{Renderer, Texture};
+use crate::render::{MaterialBuilder, PositionUvNormalVertex, Renderer, Texture};
 
 // TODO Avoid this crap, use trait objects or smth
 pub enum Material {
-    Color(ColorMaterial),
+    Color(render::Material),
     Skybox(SkyboxMaterial),
     Textured(TexturedMaterial),
     PostProcess(PostProcessMaterial),
@@ -54,19 +54,25 @@ impl Material {
 
     pub fn color(rr: &Renderer, assets: &mut Assets, color: Vec3, wireframe: bool) -> Self {
         let shader = assets.add_shader_from_file(rr, "color.wgsl");
-        // TODO We shouldn't call assets again to get the actual objects, they should be returned
-        // from the Assets' methods that created them.
-        Self::Color(ColorMaterial::new(
-            rr,
-            assets.shader(shader),
-            color,
-            wireframe,
-        ))
+        let material = MaterialBuilder::new()
+            .with_uniform_buffer(rr, WorldViewProjUniform::default())
+            .with_uniform_buffer(rr, Vec3Uniform::new(color))
+            .wireframe(wireframe)
+            // TODO Leaner vertex format. Can't use it currently because this material
+            // is used for file-loaded meshes where we currently only support a single vertex format.
+            // TODO We shouldn't call assets again to get the actual objects, they should be returned
+            // from the Assets' methods that created them.
+            .build::<PositionUvNormalVertex>(rr, assets.shader(shader));
+        Self::Color(material)
     }
 
     pub fn update(&self, rr: &Renderer, cam: &Camera, cam_tr: &Transform, tr: &Transform) {
         match self {
-            Material::Color(m) => m.set_wvp(rr, cam, cam_tr, tr),
+            Material::Color(m) => m.update_buffer(
+                rr,
+                0,
+                WorldViewProjUniform::new(&tr.matrix(), &cam_tr.view_matrix(), &cam.proj_matrix()),
+            ),
             Material::Textured(m) => m.set_wvp(rr, cam, cam_tr, tr),
             Material::Skybox(m) => m.set_wvp(rr, cam, cam_tr),
             Material::PostProcess(_) => (),
--- src/scene/materials/mod.rs
+++ src/scene/materials/mod.rs
@@ -1,4 +1,3 @@
-mod color;
 mod material;
 mod post_process;
 mod skybox;
